bb636d5cef130cbbeeb370fa06d06121f4c9996f
kvnchpl <kvnchpl@gmail.com>
2025-05-26 22:14:34 -0400

    update

--- jungle.js
+++ jungle.js
@@ -2,8 +2,15 @@ const title = document.querySelector('.centered-title');
 const body = document.body;
 
 let overlayTimeout;
+let hasInteracted = false;
 
 title.addEventListener('mouseenter', () => {
+    // Ensure image is set before the first transition
+    if (!hasInteracted) {
+        body.style.backgroundImage = 'var(--background-image-home)';
+        hasInteracted = true;
+    }
+
     // Start fading in the overlay
     body.classList.add('no-bg-image');
 
