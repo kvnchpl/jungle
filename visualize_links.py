dbb89ba5204d41d571b7c9f25656890190c6b2ab
kvnchpl <kvnchpl@gmail.com>
2025-05-26 22:39:35 -0400

    update

--- visualize_links.py
+++ visualize_links.py
@@ -6,6 +6,7 @@ LINK_RE = re.compile(r'href="z/(\d{5})')
 
 edges = []
 
+# Process files in z/
 for fname in os.listdir(Z_DIR):
     if fname.endswith('.html'):
         src = fname
@@ -14,6 +15,15 @@ for fname in os.listdir(Z_DIR):
             for match in LINK_RE.findall(html):
                 target = f"{match}.html"
                 edges.append((src, target))
+
+# Also process index.html in the root directory
+INDEX_FILE = 'index.html'
+if os.path.exists(INDEX_FILE):
+    with open(INDEX_FILE) as f:
+        html = f.read()
+        for match in LINK_RE.findall(html):
+            target = f"{match}.html"
+            edges.append((INDEX_FILE, target))
 
 with open('zine_graph.dot', 'w') as f:
     f.write('digraph Zine {\n')
