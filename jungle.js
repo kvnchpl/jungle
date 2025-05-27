c1ec63d8944f5333ffd05cc5c6dada6a9dc9ec49
kvnchpl <kvnchpl@gmail.com>
2025-05-26 21:44:11 -0400

    update

--- jungle.js
+++ jungle.js
@@ -1,9 +1,11 @@
 const title = document.querySelector('.centered-title');
 
 title.addEventListener('mouseenter', () => {
-    document.body.classList.add('title-hover-bg');
+    document.body.classList.add('fullscreen-image');
+    document.body.style.setProperty('--fullscreen-bg-image', "url('a/i/jungle_1_bw.png')");
 });
 
 title.addEventListener('mouseleave', () => {
-    document.body.classList.remove('title-hover-bg');
+    document.body.classList.remove('fullscreen-image');
+    document.body.style.removeProperty('--fullscreen-bg-image');
 });
