14df571cb87c33823ea897fd757c5d55ca7debcf
kvnchpl <kvnchpl@gmail.com>
2025-05-27 12:30:43 -0400

    update

--- visualize_links.py
+++ visualize_links.py
@@ -1,11 +1,16 @@
 import os
 import re
+from collections import defaultdict
 
 Z_DIR = 'z'
 LINK_RE = re.compile(r'href="z/(\d{5})')
+INDEX_FILE = 'index.html'
 
 edges = []
 nodes = set()
+
+in_degree = defaultdict(int)
+out_degree = defaultdict(int)
 
 # Collect all HTML files in z/
 for fname in os.listdir(Z_DIR):
@@ -17,9 +22,10 @@ for fname in os.listdir(Z_DIR):
             for match in LINK_RE.findall(html):
                 target = f"{match}.html"
                 edges.append((src, target))
+                out_degree[src] += 1
+                in_degree[target] += 1
 
-# Also process index.html in the root directory
-INDEX_FILE = 'index.html'
+# Also process index.html
 if os.path.exists(INDEX_FILE):
     nodes.add(INDEX_FILE)
     with open(INDEX_FILE) as f:
@@ -27,8 +33,10 @@ if os.path.exists(INDEX_FILE):
         for match in LINK_RE.findall(html):
             target = f"{match}.html"
             edges.append((INDEX_FILE, target))
+            out_degree[INDEX_FILE] += 1
+            in_degree[target] += 1
 
-# Add targets as nodes (in case they're not sources)
+# Add targets as nodes
 for _, tgt in edges:
     nodes.add(tgt)
 
@@ -37,7 +45,20 @@ with open('zine_graph.dot', 'w') as f:
     def label(name):
         return name.replace('.html', '')
     for node in sorted(nodes):
-        f.write(f'    "{label(node)}";\n')
+        lbl = label(node)
+        in_d = in_degree[node]
+        out_d = out_degree[node]
+
+        if in_d == 0 and out_d == 0:
+            attrs = 'color="gray", style="filled", fillcolor="lightgray"'
+        elif in_d == 0:
+            attrs = 'color="red", style="filled", fillcolor="mistyrose"'
+        elif out_d == 0:
+            attrs = 'color="blue", style="filled", fillcolor="lightblue"'
+        else:
+            attrs = ''
+        f.write(f'    "{lbl}" [{attrs}];\n')
+
     for src, tgt in edges:
         f.write(f'    "{label(src)}" -> "{label(tgt)}";\n')
     f.write('}\n')
