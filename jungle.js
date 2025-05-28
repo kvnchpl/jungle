332097f167c0c312b3236d898d60025fba6fb88b
kvnchpl <kvnchpl@gmail.com>
2025-05-27 21:16:48 -0400

    update

--- jungle.js
+++ jungle.js
@@ -1,19 +1,23 @@
 const title = document.querySelector('.centered-title');
 const body = document.body;
 
-title.addEventListener('mouseenter', () => {
-    body.classList.add('solid-bg');
-});
-title.addEventListener('mouseleave', () => {
-    body.classList.remove('solid-bg');
-});
-
-const links = document.querySelectorAll('a');
-links.forEach(link => {
-    link.addEventListener('mouseenter', () => {
+if (title) {
+    title.addEventListener('mouseenter', () => {
         body.classList.add('solid-bg');
     });
-    link.addEventListener('mouseleave', () => {
+    title.addEventListener('mouseleave', () => {
         body.classList.remove('solid-bg');
     });
-});
+}
+
+const links = document.querySelectorAll('a');
+if (links.length !== 0) {
+    links.forEach(link => {
+        link.addEventListener('mouseenter', () => {
+            body.classList.add('solid-bg');
+        });
+        link.addEventListener('mouseleave', () => {
+            body.classList.remove('solid-bg');
+        });
+    });
+}
