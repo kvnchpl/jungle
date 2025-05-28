234fe7337463874fcc665e4c3765e9bd2f8011de
kvnchpl <kvnchpl@gmail.com>
2025-05-27 23:44:57 -0400

    update

--- jungle.js
+++ jungle.js
@@ -1,6 +1,6 @@
 const body = document.body;
-const computedBg = body.style.backgroundImage;
-if (computedBg && computedBg !== 'none') {
+const inlineBg = body.getAttribute('style');
+if (inlineBg && inlineBg.includes('background-image')) {
     body.classList.add('has-inline-bg');
 }
 const title = document.querySelector('.centered-title');
